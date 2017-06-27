5909dba60a266b9bf938cc987c6e7ec7f4c861cf
Arshin Jain <arshin.jain@outlook.com>
2017-06-27 14:49:19 -0700

    Remove extraneous methods

--- SnapText/ViewController.swift
+++ SnapText/ViewController.swift
@@ -13,14 +13,11 @@ class ViewController: UIViewController {
     // Outlets
     @IBOutlet weak var imagePickerView: UIImageView!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
