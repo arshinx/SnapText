2f5d65d7fd2a6399c4cab5a2b41f83adc025fbfb
Arshin Jain <arshin.jain@outlook.com>
2017-06-27 14:35:31 -0700

    Initial Commit

---
+++ SnapText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapText
+//
+//  Created by Arshin Jain on 6/27/17.
+//  Copyright © 2017 Arshin Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
