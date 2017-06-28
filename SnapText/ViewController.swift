8937102bcd4ff0159247e3f1a22ca1624f21cdc8
Arshin Jain <arshin.jain@outlook.com>
2017-06-27 18:06:16 -0700

    Text Field Delegate

--- SnapText/ViewController.swift
+++ SnapText/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     // Outlets
     @IBOutlet weak var imagePickerView: UIImageView!
