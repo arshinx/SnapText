317231ef4f1394c7ace97868950c75e4f52a3751
Arshin Jain <arshin.jain@outlook.com>
2017-06-29 12:00:52 -0700

    Remove warnings

--- SnapText/SnapCollectionViewController.swift
+++ SnapText/SnapCollectionViewController.swift
@@ -48,13 +48,13 @@ class SnapCollectionViewController: UICollectionViewController {
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        
         return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
+        
         return 0
     }
 
