4e15c24df453145a183f844e75dd06735698c0c2
Arshin Jain <arshin.jain@outlook.com>
2017-06-29 11:44:03 -0700

    Assign snaps from delegate model

--- SnapText/SnapCollectionViewController.swift
+++ SnapText/SnapCollectionViewController.swift
@@ -18,6 +18,7 @@ class SnapCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        snaps = appDelegate.snaps
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
