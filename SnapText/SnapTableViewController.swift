86c7ae0f8f5f052dafcb9141e536c9164ac43f07
Arshin Jain <arshin.jain@outlook.com>
2017-06-29 12:19:21 -0700

    Add Edit button to Table View

--- SnapText/SnapTableViewController.swift
+++ SnapText/SnapTableViewController.swift
@@ -23,7 +23,7 @@ class SnapTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
