fceb3486ce5ac53015cbcc5b67fb32cdf0112d28
Arshin Jain <arshin.jain@outlook.com>
2017-06-29 12:20:12 -0700

    Table View - cellForRowAt indexPath

--- SnapText/SnapTableViewController.swift
+++ SnapText/SnapTableViewController.swift
@@ -38,15 +38,15 @@ class SnapTableViewController: UITableViewController {
         return snaps.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
