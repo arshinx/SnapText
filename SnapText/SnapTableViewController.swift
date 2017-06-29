32d7fbfa05cd5e48734766294f1221063dd1d76e
Arshin Jain <arshin.jain@outlook.com>
2017-06-29 12:55:12 -0700

    Reload data - Table View

--- SnapText/SnapTableViewController.swift
+++ SnapText/SnapTableViewController.swift
@@ -26,6 +26,7 @@ class SnapTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
