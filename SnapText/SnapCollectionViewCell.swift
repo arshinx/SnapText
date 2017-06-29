e68b840f92469f4b29927bf009043802f152fe8e
Arshin Jain <arshin.jain@outlook.com>
2017-06-29 13:27:03 -0700

    SnapCollectionViewCell

---
+++ SnapText/SnapCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SnapCollectionViewCell.swift
+//  SnapText
+//
+//  Created by Arshin Jain on 6/29/17.
+//  Copyright © 2017 Arshin Jain. All rights reserved.
+//
+
+import UIKit
+
+class SnapCollectionViewCell: UICollectionViewCell {
+    
+}
