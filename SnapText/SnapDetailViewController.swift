800000f604ba31339ac49b57e8132718d74ffc0c
Arshin Jain <arshin.jain@outlook.com>
2017-07-01 11:58:11 -0700

    Add Outlet - Image View

--- SnapText/SnapDetailViewController.swift
+++ SnapText/SnapDetailViewController.swift
@@ -14,6 +14,7 @@ class SnapDetailViewController: UIViewController {
     var snap: Snap!
     
     // Outlets
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
